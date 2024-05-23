343d1e77861cae94a5706555391c82cf78d576ff
Ashutosh <ashutoshnautiyal94@gmail.com>
2024-05-23 23:58:04 +0530

    initial commit

---
+++ constants.ts
@@ -0,0 +1,17 @@
+export const subscription = [
+	{
+		planType: "Basic",
+		price: "$12",
+		priceId: "price_1PGmLQSCHiAE8lflRM5K3fFh",
+	},
+	{
+		planType: "Standard",
+		price: "$20",
+		priceId: "price_1PGmLQSCHiAE8lflRM5K3fFh",
+	},
+	{
+		planType: "Premium",
+		price: "$30",
+		priceId: "price_1PGmLQSCHiAE8lflRM5K3fFh",
+	},
+];
